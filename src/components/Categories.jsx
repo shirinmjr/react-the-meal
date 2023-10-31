814479d287fcbb7bf611e3fbc1edc8666224ab4f
AnthonyBattista02 <tonybatt2002@gmail.com>
2023-10-31 15:32:38 -0500

    Tony-Test

---
+++ src/components/Categories.jsx
@@ -0,0 +1,18 @@
+
+
+export default function Categories (props) {
+
+
+    return(
+        <div>
+            <div className="categories-grid">
+                {props.meals.map((meal)=>(
+                    <div className="card" key={meal.idMeal}>
+                        <h3> {meal.strMeal} </h3>
+                        <img src={meal.strMealThumb} />
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
