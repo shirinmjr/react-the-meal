47d07f9ea275d83515b57af64ffbf57da85536f9
Shirin <shirin.mohajer@gmail.com>
2023-11-02 12:11:29 -0400

    moving components around

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,15 @@ import { useState } from 'react';
 import { AREA_URL, CATEGORY_URL } from './globals';
 import axios from 'axios';
 import './App.css';
-import Home from './components/Home';
-import Areas from './components/Areas';
-import Categories from './components/Categories';
-import SearchBar from './components/SearchBar';
+import Main from './components/Main';
 
 function App() {
-  const [areasMeals, setAreasMeals] = useState([]);
-  const [categoriesMeals, setCategoriesMeals] = useState([]);
-
-
-  const getAreasMeals = async (area) => {
-    const response = await axios.get(`${AREA_URL}${area}`);
-    console.log(response.data.meals);
-    setAreasMeals(response.data.meals);
-  };
-
-  const getCategoriesMeals = async (category) => {
-    console.log("get category function");
-    const response = await axios.get(`${CATEGORY_URL}${category}`);
-    console.log(response.data.meals);
-    setCategoriesMeals(response.data.meals);
-  };
-
   return (
-    <>
-      <Home getCategoriesMeals={getCategoriesMeals} getAreasMeals={getAreasMeals} />
-      <SearchBar />
-      <Areas areasMeals={areasMeals} />
-      <Categories categoriesMeals={categoriesMeals} />
-    </>
+    <div className='app'>
+      <div className='main-container'>
+        <Main />
+      </div>
+    </div>
   );
 }
 
--- src/components/Displays.jsx
+++ src/components/Displays.jsx
@@ -1,35 +1,35 @@
-import { useState, useEffect } from 'react'
-import Areas from './Areas'
-import Categories from './Categories'
-import { AREA_URL, CATEGORY_URL } from '../globals'
-import axios from 'axios'
+import { useState, useEffect } from 'react';
+import Areas from './Areas';
+import Categories from './Categories';
+import { AREA_URL, CATEGORY_URL } from '../globals';
+import axios from 'axios';
 
-export default function Displays (props) {
-  const [areasMeals, setAreasMeals] = useState([])
-  const [categoriesMeals, setCategoriesMeals] = useState([])
+export default function Displays(props) {
+  const [areasMeals, setAreasMeals] = useState([]);
+  const [categoriesMeals, setCategoriesMeals] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     const getAreasMeals = async () => {
-      const response = await axios.get(`${AREA_URL}${props.areaSelected}`)
-      console.log(response.data.meals)
-      setAreasMeals(response.data.meals)
-    }
-      getAreasMeals()
-  }, [props.areaSelected])
+      const response = await axios.get(`${AREA_URL}${props.area}`);
+      console.log(response.data.meals);
+      setAreasMeals(response.data.meals);
+    };
+    getAreasMeals();
+  }, [props.area]);
 
-  useEffect(()=>{
+  useEffect(() => {
     const getCategoriesMeals = async () => {
-      const response = await axios.get(`${CATEGORY_URL}${props.categorySelected}`)
-      console.log(response.data.meals)
-      setCategoriesMeals(response.data.meals)
-    }
-      getCategoriesMeals()
-  }, [props.categorySelected])
+      const response = await axios.get(`${CATEGORY_URL}${props.category}`);
+      console.log(response.data.meals);
+      setCategoriesMeals(response.data.meals);
+    };
+    getCategoriesMeals();
+  }, [props.category]);
 
   return (
     <>
-      <Areas areasMeals={areasMeals}/>
-      <Categories categoriesMeals={categoriesMeals}/>
+      <Areas areasMeals={areasMeals} />
+      <Categories categoriesMeals={categoriesMeals} />
     </>
-  )
+  );
 }
